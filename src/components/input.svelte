b85ebf9ce2871aba012d9ff939bba4e2dffdbcfd
Fatih20 <fnri39@protonmail.com>
2022-03-27 21:15:00 +0700

    Make Input.

---
+++ src/components/input.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import {
+    inputConnectedVariable,
+    unitLongPrefix,
+    unitOfComponentUsedType,
+    unitLongPrefixArray,
+    unitPrefixInformation,
+  } from "../utilities/types";
+
+  import { isInputValidFloat } from "../utilities/inputValidation";
+  export let externalValue: inputConnectedVariable;
+  export let changeExternalValue: (arg0: inputConnectedVariable) => void;
+  export let changeDtW: () => void;
+  export let currentUnitPrefix: unitLongPrefix;
+  export let currentUnit: unitOfComponentUsedType;
+
+  function handleValueChange(e: any) {
+    let newValue = e.target.value;
+
+    if (isInputValidFloat(e, newValue)) {
+      changeDtW();
+      if (newValue.length > 0) {
+        console.log(newValue);
+        changeExternalValue(parseFloat(newValue));
+      } else {
+        changeExternalValue("");
+      }
+    }
+  }
+</script>
+
+<main>
+  <input
+    type="number"
+    id="styled-input"
+    value={externalValue}
+    on:change={handleValueChange}
+  />
+  <select name="unit" id="styled-select" bind:value={currentUnitPrefix}>
+    {#each unitLongPrefixArray as prefix}
+      <option value={prefix} class="styled-option">
+        {unitPrefixInformation(prefix)[1]}
+        {`${currentUnit}`}
+      </option>
+    {/each}
+  </select>
+</main>
