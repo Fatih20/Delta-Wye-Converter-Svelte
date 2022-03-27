f6dcbd3186eca5d76f33856512b85e5cec3bc5d6
Fatih20 <fnri39@protonmail.com>
2022-03-27 15:57:38 +0700

    Header and app background.

---
+++ src/components/header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1 id="title">Delta to Wye Converter</h1>
+</main>
+
+<style>
+  main {
+    align-items: center;
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 1em 1em;
+  }
+
+  main * {
+    text-align: center;
+  }
+</style>
